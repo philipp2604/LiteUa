7f9c49275a9776602638cdbaa508c6de2c511cca
Philipp Siewert <philipp@phsiewert.de>
2025-12-04 13:02:54 +0100

    Projektdateien hinzufügen.

---
+++ src/LiteUa/Encoding/OpcUaBinaryReader.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// TODO: Fix documentation comments
+
+namespace LiteUa.Encoding
+{
+    /// <summary>
+    /// Provides methods for reading OPC UA binary-encoded data types from a stream in little-endian format.
+    /// </summary>
+    /// <param name="stream">The input stream from which binary data is read.</param>
+    public class OpcUaBinaryReader(Stream stream)
+    {
+        private readonly Stream _stream = stream ?? throw new ArgumentNullException(nameof(stream));
+
+        /// <summary>
+        /// Gets the current position within the stream.
+        /// </summary>
+        public long Position => _stream.Position;
+
+        /// <summary>
+        /// Gets the length of the stream.
+        /// </summary>
+        public long Length => _stream.Length;
+
+        public bool ReadBoolean()
+        {
+            int val = _stream.ReadByte();
+            if (val == -1) throw new EndOfStreamException();
+            return val != 0;
+        }
+
+        public byte ReadByte()
+        {
+            int val = _stream.ReadByte();
+            if (val == -1) throw new EndOfStreamException();
+            return (byte)val;
+        }
+
+        public byte[] ReadBytes(int count)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(count);
+            if (count == 0) return [];
+
+            byte[] buffer = new byte[count];
+            int totalRead = 0;
+            while (totalRead < count)
+            {
+                int read = _stream.Read(buffer, totalRead, count - totalRead);
+                if (read == 0) throw new EndOfStreamException();
+                totalRead += read;
+            }
+            return buffer;
+        }
+
+        public short ReadInt16()
+        {
+            Span<byte> buffer = stackalloc byte[2];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadInt16LittleEndian(buffer);
+        }
+
+        public ushort ReadUInt16()
+        {
+            Span<byte> buffer = stackalloc byte[2];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadUInt16LittleEndian(buffer);
+        }
+
+        public int ReadInt32()
+        {
+            Span<byte> buffer = stackalloc byte[4];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadInt32LittleEndian(buffer);
+        }
+
+        public uint ReadUInt32()
+        {
+            Span<byte> buffer = stackalloc byte[4];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadUInt32LittleEndian(buffer);
+        }
+
+        public long ReadInt64()
+        {
+            Span<byte> buffer = stackalloc byte[8];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadInt64LittleEndian(buffer);
+        }
+
+        public ulong ReadUInt64()
+        {
+            Span<byte> buffer = stackalloc byte[8];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadUInt64LittleEndian(buffer);
+        }
+
+        public float ReadFloat()
+        {
+            Span<byte> buffer = stackalloc byte[4];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadSingleLittleEndian(buffer);
+        }
+
+        public double ReadDouble()
+        {
+            Span<byte> buffer = stackalloc byte[8];
+            ReadExact(buffer);
+            return BinaryPrimitives.ReadDoubleLittleEndian(buffer);
+        }
+
+        public string? ReadString()
+        {
+            int length = ReadInt32();
+            if (length == -1) return null;
+            if (length == 0) return string.Empty;
+
+            // Maybe add max length check
+
+            byte[] bytes = ReadBytes(length);
+            return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+
+        public DateTime ReadDateTime()
+        {
+            long ticks = ReadInt64();
+            if (ticks == 0) return DateTime.MinValue;
+            try
+            {
+                return DateTime.FromFileTimeUtc(ticks);
+            }
+            catch
+            {
+                return DateTime.MinValue;
+            }
+        }
+
+        public Guid ReadGuid()
+        {
+            byte[] bytes = ReadBytes(16);
+            return new Guid(bytes);
+        }
+
+        public byte[]? ReadByteString()
+        {
+            int length = ReadInt32();
+            if (length == -1) return null;
+            if (length == 0) return [];
+
+            return ReadBytes(length);
+        }
+
+        private void ReadExact(Span<byte> buffer)
+        {
+            int totalRead = 0;
+            while (totalRead < buffer.Length)
+            {
+                int bytesRead = _stream.Read(buffer[totalRead..]);
+                if (bytesRead == 0)
+                {
+                    throw new EndOfStreamException("Unexpected end of stream");
+                }
+                totalRead += bytesRead;
+            }
+        }
+    }
+}
---
+++ src/LiteUa/Encoding/OpcUaBinaryWriter.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// TODO: Fix documentation comments
+
+namespace LiteUa.Encoding
+{
+    public class OpcUaBinaryWriter(Stream stream)
+    {
+        private readonly Stream _stream = stream ?? throw new ArgumentNullException(nameof(stream));
+
+        public long Position => _stream.Position;
+        public long Length => _stream.Length;
+
+        public void Seek(long offset, SeekOrigin origin)
+        {
+            _stream.Seek(offset, origin);
+        }
+
+        public void WriteBoolean(bool value)
+        {
+            _stream.WriteByte((byte)(value ? 1 : 0));
+        }
+
+        public void WriteByte(byte value)
+        {
+            _stream.WriteByte(value);
+        }
+
+        public void WriteBytes(byte[] value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            _stream.Write(value, 0, value.Length);
+        }
+
+        public void WriteInt16(short value)
+        {
+            Span<byte> buffer = stackalloc byte[2];
+            BinaryPrimitives.WriteInt16LittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteUInt16(ushort value)
+        {
+            Span<byte> buffer = stackalloc byte[2];
+            BinaryPrimitives.WriteUInt16LittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteInt32(int value)
+        {
+            Span<byte> buffer = stackalloc byte[4];
+            BinaryPrimitives.WriteInt32LittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteUInt32(uint value)
+        {
+            Span<byte> buffer = stackalloc byte[4];
+            BinaryPrimitives.WriteUInt32LittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteInt64(long value)
+        {
+            Span<byte> buffer = stackalloc byte[8];
+            BinaryPrimitives.WriteInt64LittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteUInt64(ulong value)
+        {
+            Span<byte> buffer = stackalloc byte[8];
+            BinaryPrimitives.WriteUInt64LittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteFloat(float value)
+        {
+            Span<byte> buffer = stackalloc byte[4];
+            BinaryPrimitives.WriteSingleLittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteDouble(double value)
+        {
+            Span<byte> buffer = stackalloc byte[8];
+            BinaryPrimitives.WriteDoubleLittleEndian(buffer, value);
+            _stream.Write(buffer);
+        }
+
+        public void WriteString(string? value)
+        {
+            if (value == null)
+            {
+                WriteInt32(-1);
+                return;
+            }
+
+            byte[] utf8Bytes = System.Text.Encoding.UTF8.GetBytes(value);
+            WriteInt32(utf8Bytes.Length);
+
+            if (utf8Bytes.Length > 0)
+            {
+                WriteBytes(utf8Bytes);
+            }
+        }
+
+        public void WriteByteString(byte[]? value)
+        {
+            if (value == null)
+            {
+                WriteInt32(-1);
+                return;
+            }
+            WriteInt32(value.Length);
+            if (value.Length > 0)
+            {
+                WriteBytes(value);
+            }
+        }
+
+        public void WriteDateTime(DateTime value)
+        {
+            if (value == DateTime.MinValue)
+            {
+                WriteInt64(0);
+                return;
+            }
+            try
+            {
+                WriteInt64(value.ToFileTimeUtc());
+            }
+            catch
+            {
+                WriteInt64(0);
+            }
+        }
+
+        public void WriteGuid(Guid value)
+        {
+            byte[] guidBytes = value.ToByteArray();
+            _stream.Write(guidBytes, 0, 16);
+        }
+    }
+}
